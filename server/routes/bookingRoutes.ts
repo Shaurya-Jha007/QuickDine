9ee73766fb915d4c55448ba2bb68f08971d72426
Shaurya Jha <jhashaurya222@gmail.com>
2026-08-06 10:39:17 +0530

    Added the bookings route

---
+++ server/routes/bookingRoutes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import {
+  createBooking,
+  getMyBookings,
+  cancelBooking,
+} from "../controllers/bookingController.js";
+import { protect } from "../middlewares/auth.js";
+
+const bookingRoutes = Router();
+
+bookingRoutes.post("/", protect, createBooking);
+
+bookingRoutes.get("/my", protect, getMyBookings);
+
+bookingRoutes.get("/:id/cancel", protect, cancelBooking);
+
+export default bookingRoutes;
--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import config from "./config/config.js";
 import authRouter from "./routes/authRoutes.js";
 import restaurantRouter from "./routes/restaurantRoutes.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/api/auth", authRouter);
 app.use("/api/restaurants", restaurantRouter);
+app.use("/api/bookings", bookingRoutes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error("Unhandled Error :", err);
